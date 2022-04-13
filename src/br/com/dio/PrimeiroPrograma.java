0875a0ad7070ee71e9a3f3d5e0d56d78f7524559
Ravena <ravena.carneiro@gmail.com>
2022-04-13 15:13:09 -0300

    First Commit Intellij

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.model.Dog;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+
+        System.out.println(dog);
+
+
+
+/*        int a = 3;
+       int b = 5;
+
+
+        System.out.println( "Hello World  " + (a+b));*/
+    }
+}
